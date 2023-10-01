dba8fd495f546478b34b72012345204f00e3bd3a
mikejbp <migueljosebp@gmail.com>
2023-10-01 17:37:18 -0400

    build: Configurate Astro boilerplate
    Preact, TypeScript, Tailwind, Next UI, Framer motion, ESLint, Prettier & Husky configuration

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), tailwind()],
+  output: "server",
+  adapter: vercel(),
+});
