576ef728399ee5d0191460eb4c5dfcb2fdbe1e0a
mikejbp <migueljosebp@gmail.com>
2023-10-01 18:01:08 -0400

    [perf]: Add Preact compat

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
-  output: "server",
-  adapter: vercel(),
+	integrations: [preact({ compat: true }), tailwind()],
+	output: 'server',
+	adapter: vercel(),
 });
